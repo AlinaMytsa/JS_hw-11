a3d2074ea802df7dd6c75756ad52a71d17c32afb
AlinaMytsa <act.mitsa@gmail.com>
2022-07-27 23:03:44 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,103 @@
+// 1
+// let a = +prompt('Enter date');
+// let date = a === 0 ? 'Верно' : 'Неверно';
+// console.log(date);
+
+//2
+// let a = +prompt('enter date');
+// let date = a > 0 ? 'Верно' : 'Неверно';
+// console.log(date);
+
+//3
+// let a = +prompt('enter date');
+// let date = a < 0 ? 'Верно' : 'Неверно';
+// console.log(date);
+
+//4
+// let a = +prompt('enter date');
+// let date = a >= 0 ? 'Верно' : 'Неверно';
+// console.log(date);
+
+//5
+// let a = +prompt('enter date');
+// let date = a <= 0 ? 'Верно' : 'Неверно';
+// console.log(date);
+
+//6
+// let a = +prompt('enter date');
+// let date = a !== 0 ? 'Верно' : 'Неверно';
+// console.log(date);
+
+//7
+// let a = prompt('enter date');
+// if(a === 'test') {
+//  alert('Верно');
+// } else {
+//   alert('Неверно');
+// }
+
+//8
+// let a = +prompt('enter date');
+// if (a === 1){
+//   alert('Верно');
+// } else {
+//   alert('Неверно');
+// }
+
+//9
+// let a = +prompt('enter date');
+// if (a > 0 && a < 5) {
+//   alert('Верно');
+// } else {
+//   alert('Неверно');
+// }
+
+//10
+// let a = +prompt('enter date');
+// if ( a === 0 || a === 2 ) {
+//   console.log( a += 7 );
+//   alert(a);
+// } else {
+//   console.log( a /= 10 );
+//   alert(a);
+// }
+
+//11
+// let a = +prompt('enter date');
+// let b = +prompt('enter date2');
+// if (a <= 1 && b >= 3) {
+//   console.log( a + b );
+// } else {
+//   console.log ( a - b );
+// }
+
+//12
+// let a = +prompt('enter date');
+// let b = +prompt('enter date2');
+// if ( (a > 2 && a < 11) || ( b >= 6 && b < 14 )) {
+//   alert('Верно');
+// } else {
+//   alert('Неверно');
+// }
+
+//13
+// let num = +prompt('enter date 1-4');
+// let result = null;
+// switch (num) {
+//   case 1 :
+//     result = 'Зима';
+//     alert(result);
+//     break;
+//   case 2 :
+//     result = 'Весна';
+//     alert(result);
+//     break;
+//   case 3 :
+//     result = 'лето';
+//     alert(result);
+//     break;
+//   case 4 :
+//     result = 'осень';
+//     alert(result);
+//     break;
+// }
